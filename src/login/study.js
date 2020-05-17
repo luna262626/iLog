ddd39830440b800be0f01ace8edb0351ccd21931
luna262626 <65507053+luna262626@users.noreply.github.com>
2020-05-17 14:56:08 -0400

    Create study.js

---
+++ src/login/study.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+
+
+import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Expo,Platform, StyleSheet, Image, Button, TouchableHighlight} from 'react-native';
+
+
+class Study extends React.Component {
+
+
+  render() {
+     
+    return (
+      <KeyboardAvoidingView style={styles.containerView} behavior="padding">
+
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.loginScreenContainer}>
+          <View style={styles.loginFormView}>
+             <TextInput placeholder="Date" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
+            
+            <TextInput placeholder="Hrs spent" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
+            
+            <TextInput placeholder="Amount of tasks done" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
+            <TextInput placeholder="Amount of subjects done" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
+             <View style={styles.textContainer}>
+            <TouchableHighlight
+              onPress={() => this.props.navigation.navigate('Saved')}
+            >
+           <View style={styles.touchableButton1}>
+                        <Text style={styles.buttonText}>
+                            Save
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    );
+  }
+
+}
+
+
+
+const styles = StyleSheet.create({
+
+containerView: {
+  flex: 1,
+},
+loginScreenContainer: {
+  flex: 1,
+  backgroundColor:'white',
+},
+textContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+loginFormView: {
+  flex: 1
+},
+loginFormTextInput: {
+  height: 43,
+  fontSize: 14,
+  borderRadius: 5,
+  borderWidth: 1,
+  borderColor: '#eaeaea',
+  backgroundColor: '#fafafa',
+  paddingLeft: 10,
+  marginLeft: 15,
+  marginRight: 15,
+  marginTop: 5,
+  marginBottom: 5,
+
+},
+
+touchableButton1: {
+        backgroundColor: '#B22222',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+    borderWidth: 1,
+    
+   
+    },
+
+ buttonText: {
+     
+        color: 'white',
+        fontSize: 14,
+        fontWeight:'bold',
+ }
+});
+
+export default Study;
